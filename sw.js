6814b1e617c7dc65a3b4ad55a1eb2837f953bc27
Immortal <eminem13981398@gmail.com>
2026-09-15 14:35:30 +0300

    Cache Control Center OAuth wrapper and core

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='selina-v3.0.1-shell';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+const CACHE='selina-v3.0.2-shell';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./app-core.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
